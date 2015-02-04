33372a7e0e6e6f76d00cd2db93713339af118d3e
jspencersharpe <jspencersharpe@gmail.com>
2015-02-04 16:20:33 -0600

    tests set up

--- Unit Test Gass-E/UnitTest1.cs
+++ Unit Test Gass-E/UnitTest1.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.IO;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Unit_Test_Gass_E
 {
     [TestClass]
-    public class UnitTest1
+    public class UITests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static TestContext test_context;
+        private static Window window;
+        private static Application application;
+
+        [ClassInitialize]
+        public static void Setup(TestContext _context)
         {
+            test_context = _context;
+            var applicationDir = _context.DeploymentDirectory;
+            var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\Unit_Test_Gass-E\\bin\\Debug\\WPFSimple");
+            application = Application.Launch(applicationPath);
+            window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+        }
+
+        [TestMethod]
+        public void TestZeroState()
+        {
+        }
+
+        [ClassCleanup]
+        public static void TearDown() 
+        {
+            window.Close();
+            application.Close();
         }
     }
 }
