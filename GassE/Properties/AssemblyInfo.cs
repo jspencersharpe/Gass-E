1f4bb9acd05e9ea02fc52f7847e176bf94fc7c5d
jspencersharpe <jspencersharpe@gmail.com>
2015-02-24 14:48:19 -0600

    removed hyphens

--- GassE/Properties/AssemblyInfo.cs
+++ GassE/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gass-E")]
+[assembly: AssemblyTitle("GassE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gass-E")]
+[assembly: AssemblyProduct("GassE")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
